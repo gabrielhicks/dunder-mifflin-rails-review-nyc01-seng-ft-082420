1c9bb2cc94f11a12654771b648e43fe8f3d1eb7b
gabrielhicks <gabrielhicks@mail.com>
2020-09-17 16:42:04 -0400

    Add new and edit view

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,4 +8,22 @@ class EmployeesController < ApplicationController
         @employee = Employee.find(params[:id])
     end 
 
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id)
+    end
+
 end
